0fa252acc5b50831a9b56eb89d08559d0d4b234d
jack-lundquist <31962755+jack-lundquist@users.noreply.github.com>
2018-02-12 20:41:44 -0500

    adding preliminary parcel data and labels

--- js/scripts_index.js
+++ js/scripts_index.js
@@ -8,87 +8,110 @@ L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
  maxZoom: 19
 }).addTo(map);
 
-var pizzaData = [
-  {
-    name: 'Chris',
-    pizzaShop: "Ben's Pizza",
-    lat: 40.730376,
-    lon: -74.0008582,
-    school: 'Wagner',
-  },
-  {
-    name: 'Maxwell',
-    pizzaShop: "Joe's",
-    lat: 40.7305876,
-    lon: -74.002141,
-    school: 'Wagner',
-  },
-  {
-    name: 'Paolo',
-    pizzaShop: "John's of Bleeker",
-    lat: 40.725717,
-    lon: -73.991492,
-    school: 'Wagner',
-  },
-  {
-    name: 'Rigel',
-    pizzaShop: "Di Fara",
-    lat: 40.6250156,
-    lon: -73.9659225,
-    school: 'Life',
-  },
-  {
-    name: 'Jack',
-    pizzaShop: "Paulie Gee's",
-    lat: 40.729662,
-    lon: -73.958579,
-    school: 'CUSP',
-  },
-  {
-    name: 'Lisanne',
-    pizzaShop: "ZuriLee",
-    lat: 40.6545,
-    lon: -73.9594,
-    school: 'Life',
-  },
-  {
-    name: 'Niki',
-    pizzaShop: "Pizza Palace",
-    lat: 40.77638,
-    lon: -73.9112052,
-    school: 'Life',
-  },
-  {
-    name: 'Monica',
-    pizzaShop: "Percy's Pizza",
-    lat: 40.72915,
-    lon: -74.001398,
-    school: 'Wagner',
-  },
-];
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.Address) {
+        layer.bindPopup(feature.properties.Address);
+    }
+}
 
-//add a marker for each object in array
-pizzaData.forEach(function(object) {
-  var latlon = [object.lat,object.lon];
-  var schoolColor = 'black';
-  if(object.school == 'Wagner') schoolColor = 'purple';
-  if(object.school == 'CUSP') schoolColor = 'green';
-  if(object.school == 'Life') schoolColor = 'orange';
-  var options = {
-    radius: 10,
-    fillColor: schoolColor,
-    weight: 6,
-    stroke: false,
-    fillOpacity: .75,
-  };
-  L.circleMarker(latlon, options).addTo(map)
-      .bindPopup(object.name + " likes to eat at " + object.pizzaShop);
-});
+var myStyle = {
+    "color": "#ff7800",
+};
 
-$('#fly_to').click(function() {
-  var randomPizza = pizzaData[(Math.floor(Math.random()*pizzaData.length))];
-  map.flyTo([randomPizza.lat, randomPizza.lon], 20);
-});
+L.geoJSON(prelim_data_one, {
+    onEachFeature: onEachFeature,
+    style: myStyle
+}).addTo(map);
+
+// prelim_data_one.forEach(function(object) {
+//   address= object.Address;
+//   marker.bindPopup(address)
+// });
+
+
+
+// var pizzaData = [
+//   {
+//     name: 'Chris',
+//     pizzaShop: "Ben's Pizza",
+//     lat: 40.730376,
+//     lon: -74.0008582,
+//     school: 'Wagner',
+//   },
+//   {
+//     name: 'Maxwell',
+//     pizzaShop: "Joe's",
+//     lat: 40.7305876,
+//     lon: -74.002141,
+//     school: 'Wagner',
+//   },
+//   {
+//     name: 'Paolo',
+//     pizzaShop: "John's of Bleeker",
+//     lat: 40.725717,
+//     lon: -73.991492,
+//     school: 'Wagner',
+//   },
+//   {
+//     name: 'Rigel',
+//     pizzaShop: "Di Fara",
+//     lat: 40.6250156,
+//     lon: -73.9659225,
+//     school: 'Life',
+//   },
+//   {
+//     name: 'Jack',
+//     pizzaShop: "Paulie Gee's",
+//     lat: 40.729662,
+//     lon: -73.958579,
+//     school: 'CUSP',
+//   },
+//   {
+//     name: 'Lisanne',
+//     pizzaShop: "ZuriLee",
+//     lat: 40.6545,
+//     lon: -73.9594,
+//     school: 'Life',
+//   },
+//   {
+//     name: 'Niki',
+//     pizzaShop: "Pizza Palace",
+//     lat: 40.77638,
+//     lon: -73.9112052,
+//     school: 'Life',
+//   },
+//   {
+//     name: 'Monica',
+//     pizzaShop: "Percy's Pizza",
+//     lat: 40.72915,
+//     lon: -74.001398,
+//     school: 'Wagner',
+//   },
+// ];
+//
+// //add a marker for each object in array
+// pizzaData.forEach(function(object) {
+//   var latlon = [object.lat,object.lon];
+//   var schoolColor = 'black';
+//   if(object.school == 'Wagner') schoolColor = 'purple';
+//   if(object.school == 'CUSP') schoolColor = 'green';
+//   if(object.school == 'Life') schoolColor = 'orange';
+//   var options = {
+//     radius: 10,
+//     fillColor: schoolColor,
+//     weight: 6,
+//     stroke: false,
+//     fillOpacity: .75,
+//   };
+//   L.circleMarker(latlon, options).addTo(map)
+//       .bindPopup(object.name + " likes to eat at " + object.pizzaShop);
+// });
+//
+// $('#fly_to').click(function() {
+//   var randomPizza = pizzaData[(Math.floor(Math.random()*pizzaData.length))];
+//   map.flyTo([randomPizza.lat, randomPizza.lon], 20);
+// });
 
 //random color generator
 function getRandomColor() {
