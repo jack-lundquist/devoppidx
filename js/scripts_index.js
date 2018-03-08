bc5c897888404f8207ecc9c2b797d129df799254
jack-lundquist <31962755+jack-lundquist@users.noreply.github.com>
2018-03-08 15:48:33 -0500

    adding user guide

--- js/scripts_index.js
+++ js/scripts_index.js
@@ -1,3 +1,7 @@
+// $(window).on('load',function(){
+//         $('#useModal').modal('show');
+//     });
+
 //map variable
 var map = L.map('my-map', {scrollWheelZoom:false}).setView([40.692874,-73.939018], 11);
 
@@ -21,34 +25,53 @@ function getColor(d) {
 
 
 function onEachFeature(feature, layer) {
-    // does this feature have a property named popupContent?
+	scores = [];
+	var idx = [$("#vacancy").val(), $("#shortTrip").val(), $("#highRent").val(), $("#renters").val(),
+	$("#owners").val(), $("#recentMove").val(), $("#unemployed").val(), $("#white").val(),
+	$("#poverty").val(), $("#elderly").val(), $("#children").val(), $("#highIncome").val(),
+	$("#hsMinus").val(), $("#collegePlus").val(), $("#oldHousing").val(), $("#newHousing").val()];
+	for (var i = 0; i < idx.length; i++) {
+		if(idx[i] > 10) {idx[i] == 10; alert('Please choose values <= 10! Value changed to 10 in index calculation.')};
+		if(idx[i] < -10) {idx[i] == -10; alert('Please choose values => -10! Value changed to -10 in index calculation.')}
+	}
+
+
+		// does this feature have a property named popupContent?
     if (feature.properties && feature.properties.Address) {
         layer.bindPopup("Address: " + feature.properties.Address + "\n" + "BBL: " + feature.properties.BBL);
     }
     feature.properties.idx = 0
-}
+		features = [layer.feature.properties.pct_vacantunits_norm, layer.feature.properties.pct_traveltime_under30mins_norm,
+		layer.feature.properties.pct_rent_2000plus_norm, layer.feature.properties.pct_renters_norm,
+		layer.feature.properties.pct_ownership_norm, layer.feature.properties.pct_movedin_2010orlater_norm,
+		layer.feature.properties.pct_unemployed_norm, layer.feature.properties.pct_white_norm,
+		layer.feature.properties.pct_inPoverty_norm, layer.feature.properties.pct_olderthan65_norm,
+		(1-layer.feature.properties.pct_olderthan18_norm), layer.feature.properties.pct_HHincomegreater100k_norm,
+		layer.feature.properties.pct_withoutHS_norm, layer.feature.properties.pct_withcollegeplus_norm,
+		layer.feature.properties.pct_builtbefore1940_norm, layer.feature.properties.pct_builtafter2010_norm];
+		var score = 0;
+		for(var i=0; i< idx.length; i++) {
+    	score += idx[i]*features[i];
+				}
+    layer.feature.properties.idx = score;
+		layer.feature.properties.color = getColor(score);
+		if (layer.feature.properties && layer.feature.properties.Address) {
+        layer.bindPopup("Address: " + layer.feature.properties.Address + "\n" + "BBL: " + layer.feature.properties.BBL
+			+ "\n" + "Index Score: " + layer.feature.properties.idx);
+    };
 
-function style(feature) {
-    return {
-      fillColor: getColor(feature.properties.idx),
+		layer.setStyle({
+			fillColor: layer.feature.properties.color,
       weight: 0,
       opacity: 0,
       color: "lightgrey",
       // dashArray: '3',
       fillOpacity: 1
-    };
-};
+		});
+}
 
 var prop_layer = L.geoJSON(data_norm, {
     onEachFeature: onEachFeature,
-    style: {
-      fillColor: "#E4D902",
-      weight: 0,
-      opacity: 0,
-      color: "lightgrey",
-      // dashArray: '3',
-      fillOpacity: 1
-    }
 });
 
 prop_layer.addTo(map);
@@ -85,16 +108,16 @@ function updateMap() {
     if(idx[i] > 10) {idx[i] == 10; alert('Please choose values <= 10! Value changed to 10 in index calculation.')};
 		if(idx[i] < -10) {idx[i] == -10; alert('Please choose values => -10! Value changed to -10 in index calculation.')}
     //Do something
-}
+	}
   prop_layer.eachLayer(function(layer, feature) {
 		features = [layer.feature.properties.pct_vacantunits_norm, layer.feature.properties.pct_traveltime_under30mins_norm,
 		layer.feature.properties.pct_rent_2000plus_norm, layer.feature.properties.pct_renters_norm,
-	layer.feature.properties.pct_ownership_norm, layer.feature.properties.pct_movedin_2010orlater_norm,
-	layer.feature.properties.pct_unemployed_norm, layer.feature.properties.pct_white_norm,
-	layer.feature.properties.pct_inPoverty_norm, layer.feature.properties.pct_olderthan65_norm,
-(1-layer.feature.properties.pct_olderthan18_norm), layer.feature.properties.pct_HHincomegreater100k_norm,
-layer.feature.properties.pct_withoutHS_norm, layer.feature.properties.pct_withcollegeplus_norm,
-layer.feature.properties.pct_builtbefore1940_norm, layer.feature.properties.pct_builtafter2010_norm];
+		layer.feature.properties.pct_ownership_norm, layer.feature.properties.pct_movedin_2010orlater_norm,
+		layer.feature.properties.pct_unemployed_norm, layer.feature.properties.pct_white_norm,
+		layer.feature.properties.pct_inPoverty_norm, layer.feature.properties.pct_olderthan65_norm,
+		(1-layer.feature.properties.pct_olderthan18_norm), layer.feature.properties.pct_HHincomegreater100k_norm,
+		layer.feature.properties.pct_withoutHS_norm, layer.feature.properties.pct_withcollegeplus_norm,
+		layer.feature.properties.pct_builtbefore1940_norm, layer.feature.properties.pct_builtafter2010_norm];
 		var score = 0;
 		for(var i=0; i< idx.length; i++) {
     	score += idx[i]*features[i];
@@ -116,15 +139,15 @@ layer.feature.properties.pct_builtbefore1940_norm, layer.feature.properties.pct_
 		});
 		scores.push(layer.feature.properties.idx);
   });
-
-	var data = JSON.stringify(data_norm);
-	data.idx = scores;
-	console.log(data);
+	var scores_addresses = prop_layer.features.map(function(feature) {
+  return [feature.properties.idx, feature.properties.Address];
+	});
+	console.log(scores_addresses);
 	// var top25_1 = prop_layer.idx.sort(function(a, b) {
   //   return b - a;
 	// }).slice(0,25);
 	// console.log(top25);
-	var top25_2 = data.sort(function(a, b) { return a.idx < b.idx ? 1 : -1; }).slice(0, 25);
+	var top25_2 = data_norm.sort(function(a, b) { return a.idx < b.idx ? 1 : -1; }).slice(0, 25);
 	console.log(top25_2);
 };
 
@@ -137,3 +160,19 @@ function resetFilters() {
 	$('input[name=farInput').val('');
 	$('input[name=farInput').placeholder("Choose a Minimum Floor Area Ratio (FAR)");
 };
+
+function applyFilters() {
+	if($('#top25').checked) {
+		var propCount = 25;
+	}
+	else if($('#top50').checked) {
+		var propCount = 50;
+	}
+	else if($('#top100').checked) {
+		var propCount = 100;
+	}
+	else{updateMap();};
+	if(propCount > 100){
+		updateMap();
+	}
+}
